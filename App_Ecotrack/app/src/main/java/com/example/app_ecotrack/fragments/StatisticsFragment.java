5eef00b5f37d31e2eb784adb603d58394f40582e
LENOVO <anhvuu1804@gmail.com>
2025-11-26 11:08:23 +0700

    Done_khaibao_StatisticsFragment

--- App_Ecotrack/app/src/main/java/com/example/app_ecotrack/fragments/StatisticsFragment.java
+++ App_Ecotrack/app/src/main/java/com/example/app_ecotrack/fragments/StatisticsFragment.java
@@ -1,4 +1,17 @@
 package com.example.app_ecotrack.fragments;
 
+import android.content.SharedPreferences;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.app_ecotrack.DatabaseHelper;
+
 public class StatisticsFragment {
+    private TextView tvLevelProgress, tvProgressPercent;
+    private ProgressBar progressLevel;
+    private LinearLayout containerCategories, containerRecent, containerWeekly;
+    private DatabaseHelper db;
+    private SharedPreferences prefs;
+    private int userId;
 }
