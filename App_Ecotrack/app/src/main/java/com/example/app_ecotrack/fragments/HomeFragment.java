50de1b42f00635806dc75d175a5e1a14d5bb7bd7
LENOVO <anhvuu1804@gmail.com>
2025-11-26 11:03:25 +0700

    Done_khaibao_HomeFragment

--- App_Ecotrack/app/src/main/java/com/example/app_ecotrack/fragments/HomeFragment.java
+++ App_Ecotrack/app/src/main/java/com/example/app_ecotrack/fragments/HomeFragment.java
@@ -1,4 +1,26 @@
 package com.example.app_ecotrack.fragments;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
 
-public class HomeFragment {
-}
+import com.example.app_ecotrack.DatabaseHelper;
+import com.example.app_ecotrack.LeaderboardActivity;
+import com.example.app_ecotrack.MainActivity;
+import com.example.app_ecotrack.R;
+import com.example.app_ecotrack.RewardsActivity;
+
+public class HomeFragment extends Fragment {
+    private TextView tvTodayPoints, tvWeekPoints, tvTotalPoints, tvTodayActivities, tvTotalActivities, tvRank;
+    private CardView cardActivities, cardRewards, cardLeaderboard;
+    private DatabaseHelper db;
+    private SharedPreferences prefs;
+    private int userId;
+
