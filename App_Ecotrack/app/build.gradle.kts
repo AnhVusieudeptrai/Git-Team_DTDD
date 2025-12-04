dbf3a7a346ede41b63b79e4c0a0559d9d6fc23bb
LENOVO <anhvuu1804@gmail.com>
2025-12-04 11:27:25 +0700

    Done_JDK

--- App_Ecotrack/app/build.gradle.kts
+++ App_Ecotrack/app/build.gradle.kts
@@ -40,6 +40,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 
 }
